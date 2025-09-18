99505c21ced5cd87617ee9bde656c2c011a236da
Merdan <wisew.tube@gmail.com>
2025-09-18 15:42:16 +0500

    Create composer.ts

---
+++ src/composer.ts
@@ -0,0 +1,22 @@
+import { Context } from "./context";
+
+export type Middleware = (ctx: Context, next: () => Promise<void>) => any;
+
+export class Composer {
+  private middlewares: Middleware[] = [];
+
+  use(mw: Middleware) {
+    this.middlewares.push(mw);
+  }
+
+  async execute(ctx: Context) {
+    const runner = async (index: number): Promise<void> => {
+      if (index < this.middlewares.length) {
+        const mw = this.middlewares[index];
+        await mw(ctx, () => runner(index + 1));
+      }
+    };
+
+    await runner(0);
+  }
+}
