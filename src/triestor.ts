31bc3f5db608500fa287d8e495759d3b836a62a2
Merdan <wisew.tube@gmail.com>
2025-09-18 14:51:45 +0500

    Update triestor.ts

--- src/triestor.ts
+++ src/triestor.ts
@@ -6,4 +6,20 @@ export class TriestorAPI {
     this.token = token;
     this.apiUrl = `https://api.telegram.org/bot${token}`;
   }
+
+  async call<T = any>(
+    method: string,
+    params: Record<string, any> = {}
+  ): Promise<T> {
+    const url = `${this.apiUrl}/${method}`;
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    });
+
+    const data = await res.json();
+    if (!data.ok) throw new Error(data.description);
+    return data.result;
+  }
 }
